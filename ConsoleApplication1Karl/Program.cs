a0afe236568b90ee0d9d6cd42db4207b26254193
Karl Hess <khess@alpha.com>
2016-05-23 15:17:01 -0700

    Added ConsoleApp

---
+++ ConsoleApplication1Karl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1Karl
+{
+    public class Program
+    {
+        //should not compile
+       public  static int add(int a, int b)
+        {
+            return a + b;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1Karl;
+using System.IO;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 1;
+            int b = 1;
+            int valid = a + b;
+            Assert.AreEqual(ConsoleApplication1Karl.Program.add(a, b), valid);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\Users\karlh\Documents\Visual Studio 2015\Projects\ConsoleApplication1Karl\TestLog.txt";
+            File.AppendAllText(path, DateTime.Now.ToShortTimeString() + Environment.NewLine);
+
+            Assert.AreEqual(true, true);
+        }
+    }
+}
